ca2130a73d509d66c8e8241b2dd31f2b6274cf53
nnorthrup <nnorthrup@NNORTHRUPSP3.bayviewglen.ca>
2016-09-13 13:45:07 -0400

    Initial Commit

---
+++ Unit One/src/com/bayviiewglen/dayfour/ExampleOne.java
@@ -0,0 +1,22 @@
+package com.bayviiewglen.dayfour;
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Example inclass
+		int digit1= (123456/100000);
+		int digit2 = (123456/10000%10);
+		int digit3 = (123456/1000%10);
+		int digit4 = (123456/100%10);
+		int digit5 = (123456/10%10);
+		int digit6 = (123456%10);
+		
+		int sum = digit1+ digit2+ digit3+ digit4 + digit5 +digit6;
+	}
+	
+	
+	
+	
+
+}
