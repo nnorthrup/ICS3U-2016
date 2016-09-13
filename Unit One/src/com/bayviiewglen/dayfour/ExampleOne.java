28cd743206ec656ecc54942a9c3776ae315680db
nnorthrup <nnorthrup@NNORTHRUPSP3.bayviewglen.ca>
2016-09-13 13:50:21 -0400

    Updated ex1 day 4

--- Unit One/src/com/bayviiewglen/dayfour/ExampleOne.java
+++ Unit One/src/com/bayviiewglen/dayfour/ExampleOne.java
@@ -4,14 +4,16 @@ public class ExampleOne {
 
 	public static void main(String[] args) {
 		//Example inclass
-		int digit1= (123456/100000);
-		int digit2 = (123456/10000%10);
-		int digit3 = (123456/1000%10);
-		int digit4 = (123456/100%10);
-		int digit5 = (123456/10%10);
-		int digit6 = (123456%10);
+		int num = 123456;
+		int digit1= (num/100000);
+		int digit2 = (num/10000%10);
+		int digit3 = (num/1000%10);
+		int digit4 = (num/100%10);
+		int digit5 = (num/10%10);
+		int digit6 = (num%10);
 		
 		int sum = digit1+ digit2+ digit3+ digit4 + digit5 +digit6;
+		System.out.println(sum);
 	}
 	
 	
